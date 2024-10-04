55804d976f0b5e74ac98164518ac4e97d0df29d9
Nahian Reza <nahianreza26@gmail.com>
2024-10-04 11:24:26 -0400

    Time: 59 ms (63.37%), Space: 16.5 MB (88.86%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,36 +1,33 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
         hmap1 = collections.defaultdict(int)
         hmap2 = collections.defaultdict(int)
 
         for i in s1:
             hmap1[i] += 1
+        
+        for j in range(len(s1)):
+            hmap2[s2[j]] += 1
 
-        length = 0
-        for i in hmap1.values():
-            length += i
 
-        l = 0
-        r = length - 1
 
-        while r < len(s2):
-            hmap2.clear()
-            for c in range(l, r + 1):
-                hmap2[s2[c]] += 1
-            if hmap1 == hmap2:
+        if hmap1 == hmap2:
+            return True
+        
+
+        for i in range(len(s1), len(s2)):
+            prev = s2[i - len(s1)]
+            hmap2[prev] -= 1
+
+            if hmap2[prev] == 0:
+                del hmap2[prev]
+            
+            cur = s2[i]
+            hmap2[cur] += 1
+
+            if hmap2 == hmap1:
                 return True
-            l += 1
-            r += 1
-            print(l)
-            print(r)
         
         return False
-
-
-        
-        
-        
-            
-
-
-        
