b0e3b5fe00420023669d831bae3b9b6bbd5bb6f4
Nahian Reza <nahianreza26@gmail.com>
2024-10-20 18:10:18 -0400

    Time: 0 ms (100%), Space: 16.5 MB (69.54%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -3,9 +3,8 @@ class Solution:
         one, two = 1, 1
 
         for i in range(n-1):
-            temp = one
-            one = one + two
-            two = temp
-        
+            one, two = one + two, one
+
         return one
+        
         
