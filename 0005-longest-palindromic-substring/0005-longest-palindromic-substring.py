0b180a95364002b42a653ef452c1163fc2f03439
Nahian Reza <nahianreza26@gmail.com>
2024-10-22 03:13:01 -0400

    Time: 224 ms (94.62%), Space: 16.8 MB (27.1%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,28 +1,37 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        maxString = ""
-        resLen = 0
-        
-        for i in range(len(s)):
-            left, right = i , i
-            while left >= 0 and right < len(s) and s[left] == s[right]:
-                if (right - left + 1) > resLen:
-                    maxString = s[left:right + 1]
-                    resLen = right - left + 1
-                left -= 1
-                right += 1
-              
-            left = i
-            right = i + 1
-            while left >= 0 and right < len(s) and s[left] == s[right]:
-                if (right - left + 1) > resLen:
-                    maxString = s[left:right + 1]
-                    resLen = right - left + 1
-                left -= 1
-                right += 1
+        #babad
 
+        maxString = ""
+        maxLen = 0
+
+        for i in range(len(s)):
+            l,r = i,i
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                l -= 1
+                r += 1
+            if r - l + 1 > maxLen:
+                maxString = s[l+ 1: r]
+                maxLen = r - l + 1
+        
+
+        
+            l,r = i,i + 1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                l -= 1
+                r += 1
+            if r - l + 1 > maxLen:
+                maxString = s[l+ 1: r ]
+                maxLen = r - l + 1
+        return maxString
+        
             
         
-        return maxString
+        
+
+        
+
+
+        
                 
         
