f9488be1b3f5b52b720b8c27fe56fd0b16571bc3
Nahian Reza <nahianreza26@gmail.com>
2024-10-22 03:43:41 -0400

    Time: 19 ms (97.14%), Space: 21.7 MB (27.05%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,18 +1,18 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        
         hmap = collections.defaultdict(list)
-           
+
         
-        for i in range(len(strs)):
-            newList = [0] * 26
-            for j in strs[i]:
-                c = ord(j) - ord('a')
-                newList[c] += 1
-            hmap[tuple(newList)].append(strs[i])
-            
-        return hmap.values()
-            
-            
+
+        for i in strs:
+            charList = [0] * 26
+            for j in i:
+                diff = ord(j) - ord('a')
+                charList[diff] += 1
+            hmap[tuple(charList)].append(i)
+        
+        return [x[1] for x in hmap.items()]
+
+        
         
         
