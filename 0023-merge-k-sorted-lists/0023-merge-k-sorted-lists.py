a608cffb3586085315b843ab1ac49ed93481ac89
Nahian Reza <nahianreza26@gmail.com>
2024-10-23 03:15:10 -0400

    Time: 43 ms (94.08%), Space: 20.2 MB (5.53%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -7,40 +7,42 @@ class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         if not lists or len(lists) == 0:
             return None
-        
-        def sortList(l1, l2):
-            dummyNode = ListNode()
-            tail = dummyNode
-            list1 = l1
-            list2 = l2
+
+        def mergeList(list1,list2):
+            dummy = ListNode()
+            tail = dummy
             
             while list1 and list2:
-            
-                if list1.val > list2.val:
-                    tail.next = list2
+                if list1.val >= list2.val:
+                    tail.next = ListNode(list2.val)
                     list2 = list2.next
-                else:
-                    tail.next = list1
+                elif list1.val < list2.val:
+                    tail.next = ListNode(list1.val)
                     list1 = list1.next
                 tail = tail.next
-        
+            
             if list1:
                 tail.next = list1
-            elif list2:
+            else:
                 tail.next = list2
-        
-            return dummyNode.next
-        
+            
+            return dummy.next
+
+
         while len(lists) > 1:
-            resList = []
-        
+            mergedList = []
             for i in range(0, len(lists), 2):
-
-                l1 = lists[i]
-                l2 = lists[i + 1] if (i + 1) < len(lists) else None
-                resList.append(sortList(l1,l2))
-
-            lists = resList
-
+                list1 = lists[i]
+                list2 = lists[i + 1] if (i + 1) < len(lists) else None
+                mergedList.append(mergeList(list1, list2))
+            lists = mergedList
+        
         return lists[0]
+            
+            
+            
+            
+            
+            
+            
         
