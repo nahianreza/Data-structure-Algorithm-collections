7777a259357e197a0a32aad9fc47af6a8eff6893
Nahian Reza <nahianreza26@gmail.com>
2024-11-03 12:30:31 -0500

    Time: 1 ms (68.43%), Space: 16.7 MB (37.9%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.py
+++ 0778-reorganize-string/0778-reorganize-string.py
@@ -1,35 +1,38 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        countMap = Counter(s)
+        count = Counter(s)
 
-        maxHeap = [[-i[1],i[0]] for i in countMap.items()]
-
-
-        q = deque()
-
-        res = ""
-        time = 0
+        maxHeap = [[-i[1], i[0]] for i in count.items()]
+        print(maxHeap)
 
         heapq.heapify(maxHeap)
 
+        q = deque()
+        res = ""
+        time = 0
+
         while maxHeap or q:
             time += 1
-            if maxHeap: 
-                val, c = heapq.heappop(maxHeap)
-                val += 1
-                res += c
-                if val != 0:
-                    q.append([[val, c], time + 1])
-            if q and q[0][1] == time:
-                new_heap = q.popleft()[0]
-                heapq.heappush(maxHeap, new_heap)
-
-        print(res)
-
+            if maxHeap:
+                val = heapq.heappop(maxHeap)
+                val[0] = val[0] + 1
+                if val[0] != 0:
+                    q.append([val, time + 1])
+                res += val[1]
+            if q:
+                if q[0][1] == time:
+                    temp = q.popleft()[0]
+                    heapq.heappush(maxHeap, temp)
+                
         for i in range(1,len(res)):
-            if res[i-1] == res[i]:
+            if res[i] == res[i-1]:
                 return ""
         else:
             return res
+
+                
+
+
+        
 
 
