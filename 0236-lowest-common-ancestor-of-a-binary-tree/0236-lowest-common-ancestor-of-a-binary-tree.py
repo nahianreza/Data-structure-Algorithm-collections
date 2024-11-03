c851b014f9034f41d0e6321215a2d1baf9b36fe2
Nahian Reza <nahianreza26@gmail.com>
2024-11-03 17:02:24 -0500

    Time: 51 ms (50.33%), Space: 21.2 MB (41.92%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -6,32 +6,22 @@
 #         self.right = None
 
 class Solution:
-    #if node.left and node.right under node: then Lca is node
-    # if node.right under node.left: then Lca is node.left
-    #if node.left under node.right: then Lca is node.right
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if not p:
-            return q
-        if not q:
-            return p
-        if not p and not q:
+        if not root:
             return None
+        if root == p or root == q:
+            return root
         
+        l = self.lowestCommonAncestor(root.left, p, q)
+        r = self.lowestCommonAncestor(root.right, p, q)
         
-        
-        def dfs(curr):
-            if curr == p or curr == q:
-                return curr
+
+        if l and r:
+            return root
+        else:
+            return l or r
+
             
-            l = dfs(curr.left) if curr.left else None
-            r = dfs(curr.right) if curr.right else None
-            
-            if l and r:
-                return curr
-            else:
-                return l or r
-                
-                
+    
         
-        return dfs(root)
-            
+        
