aac1c6dac82530c6a86d0a6692a4172e5ce80d8b
Nahian Reza <nahianreza26@gmail.com>
2024-11-07 21:47:54 -0500

    Time: 115 ms (79.42%), Space: 16.6 MB (68.93%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,6 +1,7 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
         count = 0
+        max_count = 0
 
         for i in range(len(s)):
             l, r = i, i
@@ -8,13 +9,15 @@ class Solution:
             while l >= 0 and r < len(s) and s[l] == s[r]:
                 count += 1
                 l -= 1
-                r += 1
+                r+= 1
 
             l, r = i, i + 1
 
             while l >= 0 and r < len(s) and s[l] == s[r]:
                 count += 1
                 l -= 1
-                r += 1
-        
+                r+= 1 
+
         return count
+            
+        
